ebe06adcb44bc1e10546c4e7e61897762636e337
Sergii Marchenko <marikmarchelo@gmail.com>
2024-04-26 12:22:00 +0300

    add commit

---
+++ src/js/02-video.js
@@ -0,0 +1,18 @@
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// player.on(
+//   'timeupdate',
+//   throttle(function (data) {
+//     localStorage.setItem('videoplayer-current-time', data.seconds);
+//   }, 1000)
+// );
+
+// const getTimeVideo = localStorage.getItem('videoplayer-current-time');
+
+// if (getTimeVideo || 0) {
+//   player.setCurrentTime(getTimeVideo);
+// }
---
+++ src/js/03-feedback.js
@@ -0,0 +1,42 @@
+// import throttle from 'lodash.throttle';
+// const form = document.querySelector('.feedback-form');
+
+// form.addEventListener('input', throttle(onFormInput, 500));
+// form.addEventListener('submit', onFormSubmit);
+
+// const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+// setInputOnForm();
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   if (form.elements.email.value === '' || form.elements.message.value === '') {
+//     return alert('Please fill in all the fields!');
+//   }
+
+//   console.log({
+//     email: form.elements.email.value,
+//     message: form.elements.message.value,
+//   });
+//   event.currentTarget.reset();
+//   localStorage.removeItem(LOCAL_STORAGE_KEY);
+// }
+
+// function onFormInput(event) {
+//   const formData = {
+//     email: form.elements.email.value,
+//     message: form.elements.message.value,
+//   };
+
+//   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+// }
+
+// function setInputOnForm() {
+//   const savedMessage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+//   if (savedMessage) {
+//     form.elements.email.value = savedMessage.email;
+//     form.elements.message.value = savedMessage.message;
+//   }
+// }
