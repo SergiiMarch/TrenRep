4f5333eaa90e56e8399c023a83493ac7bc1b1e2f
Sergii Marchenko <marikmarchelo@gmail.com>
2024-05-29 11:01:41 +0300

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -46,14 +46,46 @@ const formEl = document.querySelector('.feedback-form');
 
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
-formEl.addEventListener('input', throttle(handlerClick, 1000));
+setInputOnForm();
 
-function handlerClick(e) {
+formEl.addEventListener('input', throttle(onFormInput, 1000));
+formEl.addEventListener('submit', onFormSubmit);
+
+function setInputOnForm() {
+  const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (savedData) {
+    const { email, message } = JSON.parse(savedData);
+    formEl.elements.email.value = email || '';
+    formEl.elements.message.value = message || '';
+  }
+}
+
+function onFormSubmit(e) {
   e.preventDefault();
+
+  if (
+    formEl.elements.email.value === '' ||
+    formEl.elements.message.value === ''
+  ) {
+    return alert('Please fill in all the fields!');
+  }
+
+  const formData = {
+    email: formEl.elements.email.value,
+    message: formEl.elements.message.value,
+  };
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+  console.log(formData);
+
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+function onFormInput() {
   const formData = {
     email: formEl.elements.email.value,
     message: formEl.elements.message.value,
   };
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
-  console.log(formData);
 }
